74301c5b799a91aed40d07504d686fc9e5e0cf6c
Pontus Abrahamsson <pontus.abrahamsson@mtgx.se>
2015-12-25 00:52:11 +0100

    Update css

--- src/server.js
+++ src/server.js
@@ -8,7 +8,10 @@ const { PORT } = config;
 new WebpackDevServer(webpack(wConfig), {
   publicPath: wConfig.output.publicPath,
   hot: true,
-  historyApiFallback: true
+  historyApiFallback: true,
+  stats: {
+    colors: true,
+  },
 }).listen(PORT, 'localhost', (err) => {
   if (err) console.log(err);
   console.log(`Server started on port ${PORT}`);
