c64e686016fa1cae99703f00a225c41e1576eaf1
Pontus Abrahamsson <pontus.abrahamsson@mtgx.se>
2015-12-25 23:45:17 +0100

    Updated dependencies

--- api/service/db.js
+++ api/service/db.js
@@ -1,8 +1,7 @@
 // import Promise from 'bluebird';
 // import Parse from 'parse/node';
-// import config from '../../src/config';
 
-// const { PARSE_APP, PARSE_KEY } = config;
+// const { PARSE_APP, PARSE_KEY } = process.env;
 
 // export default {
 //   save(data) {
