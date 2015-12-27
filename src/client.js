3085c4d0d27c615a6c38ca03de38a2f1e563c830
Pontus Abrahamsson <pontus.abrahamsson@mtgx.se>
2015-12-27 12:04:31 +0100

    Update dependencies

--- src/client.js
+++ src/client.js
@@ -2,15 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from 'react-router';
 import { Provider } from 'react-redux';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes.jsx';
 import configureStore from './configureStore';
 import './sass/main.scss';
 
 const store = configureStore();
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router history={history}>
       {routes}
     </Router>
   </Provider>,
