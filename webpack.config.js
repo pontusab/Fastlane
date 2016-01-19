432f18d57cfe5ec03a35c9b473753c1b5c127515
Pontus Abrahamsson <pontus.abrahamsson@mtgx.se>
2016-01-19 20:57:50 +0100

    Update webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,55 @@
-var webpack = require('webpack');
-var config = require('./config');
-var path = require('path');
+const webpack = require('webpack');
+const config = require('./config');
+const path = require('path');
 
 module.exports = {
   entry: [
-    'webpack-hot-middleware/client?path=http://localhost:'+ config.PORT +'/__webpack_hmr',
+    'webpack-hot-middleware/client?path=http://localhost:' + config.PORT + '/__webpack_hmr',
     './src/client',
   ],
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
-    publicPath: '/public/'
+    publicPath: '/public/',
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        sequences: true,
+        dead_code: true,
+        drop_debugger: true,
+        comparisons: true,
+        conditionals: true,
+        evaluate: true,
+        booleans: true,
+        loops: true,
+        unused: true,
+        hoist_funs: true,
+        if_return: true,
+        join_vars: true,
+        cascade: true,
+        drop_console: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new webpack.optimize.DedupePlugin(),
+  ],
   module: {
     loaders: [
       { test: /\.jsx?$/, loaders: ['babel'], include: path.join(__dirname, 'src') },
       { test: /\.scss$/, loader: 'style!css!sass' },
       { test: /\.json$/, loader: 'json' },
       { test: /\.woff2?($|\?)/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-    ]
+    ],
   },
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['', '.js'],
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-  ]
 };
-
-
