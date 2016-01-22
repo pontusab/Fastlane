262b5aec13f615afdf5009c963e991432f28826e
Pontus Abrahamsson <pontus.abrahamsson@mtgx.se>
2016-01-22 14:36:04 +0100

    Aded icons

--- menubar/app.js
+++ menubar/app.js
@@ -1,7 +1,16 @@
-/* eslint strict: 0 */
-var menubar = require('menubar');
+/* We need servers to run the app. */
+require('../index');
 
-var mb = menubar({
+const electron = require('electron');
+const menubar = require('menubar');
+const path = require('path');
+const Tray = electron.Tray;
+
+console.log(Tray);
+
+menubar({
+  icon: path.join(__dirname, 'Icon@2x.png'),
+  // preloadWindow: true,
   transparent: true,
   height: 525,
   width: 340,
